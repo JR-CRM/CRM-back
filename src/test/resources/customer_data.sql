4d1e3d8c587dfdf5216b69916f87e62848553fe1
Nickolay <n-deryagin@bk.ru>
2021-03-21 18:43:53 +0300

    Nickolay: Creating 2 scripts for testing, customer_data and customer_schema

---
+++ src/test/resources/customer_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMER(ID, NAME, EMAIL, PHONENUMBER)
+VALUES (1, 'Peter', '+79001582323' 'pet@mail.ru'),
+       (2, 'Vasil', '+79001584545' 'v@mail.ru'),
+       (3, 'Serg', '+79003402023' 'serg@mail.ru');
