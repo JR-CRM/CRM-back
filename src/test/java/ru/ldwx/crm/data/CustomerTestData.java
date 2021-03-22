ef9fcd909ba894dc39af7545a5fad2758c01516e
Nickolay <n-deryagin@bk.ru>
2021-03-22 10:45:48 +0300

    Nickolay: replace phone -> phonenumber int scripts, creating tests -> CustomerTestData, CustomerRepositoryImplTest, modified -> customer_data, customer_schema. Issues are constructors and toString()

---
+++ src/test/java/ru/ldwx/crm/data/CustomerTestData.java
@@ -0,0 +1,32 @@
+package ru.ldwx.crm.data;
+
+import ru.ldwx.crm.model.CustomerEntity;
+import ru.ldwx.crm.model.PersonDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerTestData {
+
+    public static Optional<CustomerEntity> getCustomerWithFirstId() {
+        return Optional.of(new CustomerEntity(1, "Peter", "+79001582323", "pet@mail.ru"));
+    }
+
+    public static Optional<CustomerEntity> getCustomerByEmail() {
+        return Optional.of(new CustomerEntity(1, "Peter", "+79001582323", "pet@mail.ru"));
+    }
+
+    public static List<CustomerEntity> getCustomersByName() {
+        return List.of(new CustomerEntity(1, "Peter", "+79001582323", "pet@mail.ru"));
+    }
+
+    public static List<CustomerEntity> getCustomersByPhoneNumber() {
+        return List.of(new CustomerEntity(1, "Peter", "+79001582323", "pet@mail.ru"));
+    }
+
+//    public static List<PersonDto> getAllPersonDtos() {
+//        return List.of(new PersonDto(1L, "Peter", 18),
+//                new PersonDto(2L, "Vasil", 22),
+//                new PersonDto(3L, "Serg", 25));
+//    }
+}
