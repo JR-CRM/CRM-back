ee0be74d3a401592feab8ac67f36171bcb8b4885
Nickolay <n-deryagin@bk.ru>
2021-03-23 20:51:42 +0300

    Nickolay: Add two sql scrypts, add StatusDto, add StatusEntity, add test on empty list

---
+++ src/main/java/ru/ldwx/crm/model/StatusDto.java
@@ -0,0 +1,42 @@
+package ru.ldwx.crm.model;
+
+import java.util.Objects;
+
+public class StatusDto {
+    private int code;
+    private String name;
+
+    public StatusDto(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StatusDto statusDto = (StatusDto) o;
+        return code == statusDto.code && Objects.equals(name, statusDto.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, name);
+    }
+}
---
+++ src/test/java/ru/ldwx/crm/repository/DictionaryRepositoryImplTest.java
@@ -0,0 +1,39 @@
+package ru.ldwx.crm.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@JdbcTest
+@TestPropertySource("classpath:sql-query.properties")
+@Sql(scripts = {"classpath:dictionary_schema.sql", "classpath:dictionary_data.sql"})
+class DictionaryRepositoryImplTest {
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    private DictionaryRepository repository;
+
+    @BeforeEach
+    void init() {
+        repository = new DictionaryRepositoryImpl(jdbcTemplate);
+    }
+
+    @Test
+    void getAllStatusesShouldWork() {
+    }
+
+    @Test
+    void getAllStatusesShouldBeNull() {
+        List<String> allStatuses = repository.getAllStatuses();
+        assertTrue(allStatuses.isEmpty());
+    }
+}
