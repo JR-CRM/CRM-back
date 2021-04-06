58160156adf95c58d8f7d5514226491e43735947
Yulia Platonova <julia.ikkunaprincessa@gmail.com>
2021-04-06 10:53:31 -0700

    Task29(refactoring of some tests)

--- src/test/java/ru/ldwx/crm/data/StatusTestData.java
+++ src/test/java/ru/ldwx/crm/data/StatusTestData.java
@@ -10,19 +10,23 @@ public class StatusTestData {
     public static List<StatusEntity> getAllStatusEntities() {
         return List.of(new StatusEntity("1", "Новый"),
                 new StatusEntity("2", "В работе"),
-                new StatusEntity("3", "Завершен"));
+                new StatusEntity("3", "Завершен"),
+                new StatusEntity("4", "Отказ"))
+        ;
     }
 
     public static List<StatusDto> getAllStatusDtos() {
         return List.of(new StatusDto("1", "Новый"),
                 new StatusDto("2", "В работе"),
-                new StatusDto("3", "Завершен"));
+                new StatusDto("3", "Завершен"),
+                new StatusDto("4", "Отказ"));
     }
 
     public static List<StatusDto> getAllStatusDtoFilters() {
         return List.of(new StatusDto("All", "Statuses"),
                 new StatusDto("1", "Новый"),
                 new StatusDto("2", "В работе"),
-                new StatusDto("3", "Завершен"));
+                new StatusDto("3", "Завершен"),
+                new StatusDto("4", "Отказ"));
     }
 }
