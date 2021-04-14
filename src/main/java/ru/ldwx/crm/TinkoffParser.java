1262ba593431b01ddf773bdafeab60d5237f4097
Pavel Gusev <65807394+Gusev2048@users.noreply.github.com>
2021-04-14 18:08:37 +0300

    Update TinkoffParser.java

--- src/main/java/ru/ldwx/crm/TinkoffParser.java
+++ src/main/java/ru/ldwx/crm/TinkoffParser.java
@@ -8,25 +8,26 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
 /**
  * Парсер api.tinkoff.ru с целью получения курса доллара.
  * api.tinkoff.ru предоставляет свежие обменные курсы. Периодичность обновления - раз в минуту (вероятно)
  *
- * метод getUsdToRub() - покупаем доллары за рубли у банка
- * метод getRubToUsd() - покупаем рубли за доллары у банка
+ * метод getCostUsdBuy() - покупаем доллары за рубли у банка
+ * метод getCostUsdSell() - покупаем рубли за доллары у банка
  *
  * !!!МЕТОДЫ КИДАЮТ ОШИБКИ!!!
- * !!!ИНОГДА (нерегулярно) НОВЫЙ ОБЪЕКТ ВОЗВРАЩАЕТ null!!!
  *
  * TODO: Дописать обработчик и возврат false
  *
  * Добавлен таймер на обновление. Теперь метод не будет дергать тинькофф чаще раза в 30 секунд.
  */
 
+@Service
 public class TinkoffParser {
 
-    private static LocalDateTime lastUsdCostUpdateTime; //возраст курса доллара
+    private LocalDateTime lastUsdCostUpdateTime; //возраст курса доллара
     private BigDecimal costUsdBuy; //курс доллара
     private BigDecimal costUsdSell; //обратный курс
 
@@ -58,12 +59,12 @@ public class TinkoffParser {
         return costUsdSell;
     }
 
-    private static synchronized boolean timeChecker (){
+    private boolean timeChecker (){
 
         LocalDateTime nowDateTime = LocalDateTime.now(); //получаем текущее время системы nowDateTime
         Duration p = Duration.between(lastUsdCostUpdateTime, nowDateTime); //получаем разницу между последним обновлением курса usd и текущей nowDateTime
 
-        return Math.abs(p.getSeconds()) > 30L; //если разница p превышает 30 секунд, то возвращаем false
+        return p.getSeconds() > 30L; //если разница p превышает 30 секунд, то возвращаем false
     }
 
     private void usdReNewer () throws Exception{ //обновлятель курса доллара и его возраста
@@ -91,9 +92,11 @@ public class TinkoffParser {
             if(a.get("category").equals("DebitCardsTransfers") & a.getJSONObject("toCurrency").get("name").equals("RUB") & a.getJSONObject("fromCurrency").get("name").equals("USD")){
                 if (a.has("buy")){
                     costUsdBuy = new BigDecimal(a.get("buy").toString());
+                    //System.out.println("buy " + usdToRub);
                 }
                 if (a.has("sell")){
                     costUsdSell = new BigDecimal(a.get("sell").toString());
+                    //System.out.println("sell " + rubToUsd);
                 }
             }
         }
